f4d438846248c49b049ff97e67242b072865e9a7
Keith Strini <kstrini@pivotal.io>
2016-02-21 12:44:54 -0800

    Filled out Robust Software Delivery Pipeline

---
+++ ci/tasks/build-artifact.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+export GRADLE_OPTS=-Dorg.gradle.native=false
+version=`cat version/number`
+cd code-repo
+gradle assemble -PVersion=$version
---
+++ ci/tasks/cf-deployment.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -x
+
+target="cf api $API_ENDPOINT --skip-ssl-validation"
+echo $target
+eval $target
+
+echo "Login....."
+login="cf auth $USERNAME $PASSWORD"
+echo $login
+eval $login
+
+echo "Target Org and Space"
+org_space="cf target -o $ORG -s $SPACE"
+eval $org_space
+
+echo "push the app"
+push="cf push $CODE_NAME -n $HOST -p code-release/simple-mongodb-rest-app-*.war -m 1GB --no-manifest cf bind-service p-mongodb"
+echo $push
+eval $push
+
+cf bind-service $CODE_NAME p-mongodb
