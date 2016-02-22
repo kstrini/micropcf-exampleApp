e89dfd4c8ca024bc2714170e23caec05a5deef9e
Keith Strini <kstrini@pivotal.io>
2016-02-21 18:11:42 -0800

    Switched backed to assemble

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -5,4 +5,4 @@ export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 cd code-repo
 gradle assemble -PVersion=$version
-rm -rf build/libs/simple-mongodb-rest-app-$version.war.original
+echo ls build/libs/
