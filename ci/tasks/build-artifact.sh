3b6fa0d18ab32ebe1c8c79cdf8a47ef1f9db94dc
Keith Strini <kstrini@pivotal.io>
2016-02-21 18:18:03 -0800

    Switched backed to assemble

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -5,4 +5,11 @@ export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 cd code-repo
 gradle assemble -PVersion=$version
-echo ls build/libs/
+
+echo "WhereAmi"
+locate="pwd"
+eval $locate
+
+echo "Where do I want"
+findwar="ls build/libs/"
+eval $findwar
