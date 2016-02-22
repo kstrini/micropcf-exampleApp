a7358ebcb11a2cf3797ab8f0976625c8c4b9a6c3
Keith Strini <kstrini@pivotal.io>
2016-02-21 17:23:34 -0800

    Remove *.original artifact

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -5,3 +5,4 @@ export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 cd code-repo
 gradle assemble -PVersion=$version
+rm -rf build/libs/simple-mongodb-rest-app-.*.war.original
