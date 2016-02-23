a3157946f00a77c5db556d471291e1017bb33761
Keith Strini <kstrini@pivotal.io>
2016-02-22 21:43:46 -0500

    Fixed Backup Source issue generating *.war.original

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -5,7 +5,3 @@ export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 cd code-repo
 gradle assemble -PVersion=$version
-
-echo "Removing Original War"
-delWar="rm -rf build/libs/simple-mongodb-rest-app-*.war.original"
-eval $delWar
