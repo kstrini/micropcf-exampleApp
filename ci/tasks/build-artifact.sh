351cd448ea5628eaf02c417370c4871ab85c48cf
Keith Strini <kstrini@pivotal.io>
2016-03-01 15:31:40 -0500

    Fixed to be Compliant with Concourse Update

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -2,6 +2,11 @@
 set -e
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
+BINARY_DIR=$PWD/binary
+
 version=`cat version/number`
 cd code-repo
 gradle assemble -PVersion=$version
+
+mv build/libs/*.war $BINARY_DIR
+ls $BINARY_DIR
