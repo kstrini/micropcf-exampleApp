171cab6684d47d33a926a9eced44f48ad49c90ff
Keith Strini <kstrini@pivotal.io>
2016-03-02 13:29:14 -0500

    reverted to original script

--- ci/tasks/cats.sh
+++ ci/tasks/cats.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-sleep 1m
 get_code="curl -I $CODE_URL 2>/dev/null | head -n 1 | cut -d$' ' -f2"
-echo $get_code
-status_code=eval $get_code
-echo $status_code
-if [ "$status_code" != '204' ]
+status_code=`eval $get_code`
+if [ "$status_code" != "204" ]
 then
   echo "Expect status code from $CODE_URL as 204, but got $status_code"
   exit 1
