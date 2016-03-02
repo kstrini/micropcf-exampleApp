4696d17405ede3d1892c40f8df1dacf66ef20d59
Keith Strini <kstrini@pivotal.io>
2016-03-02 11:43:48 -0500

    Adding echo for debug

--- ci/tasks/cats.sh
+++ ci/tasks/cats.sh
@@ -3,8 +3,10 @@ set -e
 
 sleep 1m
 get_code="curl -I $CODE_URL 2>/dev/null | head -n 1 | cut -d$' ' -f2"
+echo $get_code
 status_code=`eval $get_code`
-if [ "$status_code" != 204 ]
+echo $status_code
+if [ "$status_code" != '204' ]
 then
   echo "Expect status code from $CODE_URL as 204, but got $status_code"
   exit 1
