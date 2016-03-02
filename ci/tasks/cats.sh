29a29b838a03ae086509edb0856952a725084b8b
Keith Strini <kstrini@pivotal.io>
2016-03-02 12:55:51 -0500

    reduced attempts to 3

--- ci/tasks/cats.sh
+++ ci/tasks/cats.sh
@@ -4,7 +4,7 @@ set -e
 sleep 1m
 get_code="curl -I $CODE_URL 2>/dev/null | head -n 1 | cut -d$' ' -f2"
 echo $get_code
-status_code="eval $get_code"
+status_code=eval $get_code
 echo $status_code
 if [ "$status_code" != '204' ]
 then
